206bb4326fa9c9bfb86ea9ceb2f9268c9f6a2414
peterwinkley <pgwinkley@gmail.com>
2025-08-19 14:11:48 +0100

    add intune script for detecting secure boot cert

---
+++ CheckSecureBootIntune.ps1
@@ -0,0 +1,9 @@
+﻿
+
+$Sboot = [System.Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI db).bytes) -match ‘Windows UEFI CA 2023’
+
+Write-Host $Sboot
+
+if ($Sboot -eq $true) {
+    Write-Output "Windows UEFI CA 2023 certificate found." > C:\SecureBoot.txt
+}
